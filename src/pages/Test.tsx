c6e4954b484d2a00512586f0efd7d8b82c38528e
Kuttapat Somwang <arrliver@gmail.com>
2024-01-18 10:05:57 +0700

    hello

---
+++ src/pages/Test.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react"
+import { Column } from "./types"
+
+const Test = () => {
+
+    const [columns , setColumns] = useState<Column[]>([]);
+    console.log(columns)
+    const createNewColumn = () =>{
+        const columnToAdd : Column  = {
+            id:generateId(),
+            title:`Column ${columns.length + 1}`
+        }
+
+        setColumns([...columns,columnToAdd])
+    }
+
+    return (
+        <div>
+            <button onClick={createNewColumn}>Button</button>
+            {columns.map(i=>{
+                return <div>{i.title}</div>
+            })}
+        </div>
+    )
+
+}
+
+const generateId = () =>{ 
+    return Math.floor(Math.random() * 10001)
+}
+
+
+
+export default Test
